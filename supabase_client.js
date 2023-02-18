67b50e3a26c1bd705ff9194aa16e635828540959
ianng1 <iyhn8192@gmail.com>
2023-02-17 21:01:11 -0800

    fixing small error

--- supabase_client.js
+++ supabase_client.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import 'react-native-url-polyfill'
+import 'react-native-url-polyfill/auto'
 
 
 const supabaseUrl = 'https://jdkwthcuizrwzcpulkex.supabase.co/'
