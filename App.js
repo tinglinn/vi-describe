a6faab3a5543d933b04c9c4cfab1bc7668887659
ting <tinglin0708@gmail.com>
2023-02-17 18:55:16 -0800

    set up skeleton

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, Dimensions, Button } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, BottomTabBar, BottomTabView } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+
+import SwipeScreen from './pages/swipe';
+import UploadScreen from './pages/upload';
+import GalleryScreen from './pages/gallery';
+import NotifsScreen from './pages/notifs';
+import ProfileScreen from './pages/profile';
+
+const Tab = createBottomTabNavigator();
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator>
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
