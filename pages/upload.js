6fb8227830528ca825fe4207192446efb42010f9
ting <tinglin0708@gmail.com>
2023-02-18 19:47:18 -0800

    frontend for upload page

--- pages/upload.js
+++ pages/upload.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Image, StyleSheet } from 'react-native';
+import { Text, View, TextInput, Button, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { supabase } from '../supabase_client';
 import { Upload } from 'upload-js'
+import Themes from '../assets/themes';
 export default function ImageUpload () {
     const [image, setImage] = useState(null);
     const [prompt, setPrompt] = useState('');
@@ -59,40 +60,116 @@ export default function ImageUpload () {
 
     return (
         <View style={styles.container}>
-            {image && (
-                <Image source={{ uri: image }} style={styles.image} />
-            )}
-            <Button title="Choose an image" onPress={pickImage} />
-            <TextInput
-                style={styles.promptInput}
-                onChangeText={text => setPrompt(text)}
-                value={prompt}
-                placeholder="Enter a prompt"
-            />
-            <Button title="Submit" onPress={() => console.log(prompt, image)} />
+
+            <View style={styles.uploadCard}>
+                <Text style={styles.uploadText}>Upload</Text>
+                <Text style={styles.reminder}>Choose an artwork that you would like others to describe.</Text>
+                <View style={{ width: '100%', alignItems: 'center' }}>
+                    <TouchableOpacity onPress={pickImage} style={styles.uploadBox}>
+                        {!image && <Text style={[styles.reminder, { color: 'lightgray' }]}>Browse to chose an image</Text>}
+                        {image && <Image source={{ uri: image }} style={styles.image} />}
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.promptBox}>
+                <Text style={styles.uploadText}>Text</Text>
+                <Text style={styles.reminder}>Optionally enter a prompt - what does this artwork mean to you? What would you like people to focus on?</Text>
+                <TextInput
+                    style={styles.promptInput}
+                    onChangeText={text => setPrompt(text)}
+                    value={prompt}
+                    placeholder="Enter your prompt"
+                    multiline={true}
+                    maxLength={100}
+                    />
+            </View>
+            <TouchableOpacity onPress={() => Alert.alert("Successfully posted!")}>
+                <View style={{ borderRadius: 5, marginTop: 20, width: 80, height: 40, backgroundColor: '#be3577', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{fontFamily: 'Poppins-SemiBold', fontSize: 14, color: 'white'}}>Submit</Text>
+                </View>
+            </TouchableOpacity>
+            
         </View>
     );
 };
+
+//<Button title="Submit" onPress={() => console.log(prompt, image)} />
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: Themes.colors.blue
+        //backgroundColor: '#9bc2ff'
+    },
+    titleText: {
+        fontFamily: "Poppins-SemiBold",
+        color: 'white',
+        fontSize: 24,
+        textAlign: 'center'
+    },
+    subtitle: {
+        fontFamily: "Poppins",
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    uploadCard: {
+        width: '90%',
+        borderRadius: 5,
+        backgroundColor: Themes.colors.white,
+        padding: 20,
+        marginTop: 30,
+        //justifyContent: 'space-between',
+    },
+    uploadText: {
+        fontFamily: "Poppins-SemiBold",
+        fontSize: 22,
+        color: Themes.colors.black
+    },
+    uploadBox: {
+        width: '100%',
+        height: 150,
+        borderRadius: 2,
+        borderWidth: 1,
+        borderColor: Themes.colors.verydark,
+        borderStyle: "dashed",
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     image: {
-        width: 200,
-        height: 200,
+        width: '100%',
+        height: '100%',
         resizeMode: 'cover',
         marginBottom: 20,
     },
+    reminder: {
+        fontFamily: 'Poppins',
+        fontSize: 14,
+        color: Themes.colors.black,
+    },
+    promptBox: {
+        width: '90%',
+        borderRadius: 5,
+        backgroundColor: Themes.colors.white,
+        padding: 20,
+        marginTop: 20,
+    },
     promptInput: {
-        width: '80%',
+        width: '100%',
         height: 50,
         borderWidth: 1,
-        borderColor: 'gray',
+        borderColor: Themes.colors.verydark,
+        borderStyle: 'dashed',
+        borderRadius: 2,
         padding: 10,
         marginBottom: 20,
+        marginTop: 15,
+        textAlign: 'center',
+        fontFamily: 'Poppins',
+        color: 'gray'
     },
 });
 
